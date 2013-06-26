a182f574bda070ed62f8e190dfc125eec23d3328
mecitsari <devnull@localhost>
2013-06-26 13:53:00 +0300

    functions are added correctly

---
+++ portal/src/main/webapp/js/network/common-functions.js
@@ -0,0 +1,460 @@
+/**
+ * Toggles the visibility of the pan/zoom control panel.
+ */
+function _togglePanZoom(self)
+{
+    // update visibility of the pan/zoom control
+
+    self._panZoomVisible = !self._panZoomVisible;
+
+    self._vis.panZoomControlVisible(self._panZoomVisible);
+
+    // update check icon of the corresponding menu item
+
+    var item = $(self.mainMenuSelector + " #show_pan_zoom_control");
+
+    if (self._panZoomVisible)
+    {
+        item.addClass(self.CHECKED_CLASS);
+    }
+    else
+    {
+        item.removeClass(self.CHECKED_CLASS);
+    }
+}
+
+/**
+ * This function is designed to be invoked after an operation (such as filtering
+ * nodes or edges) that changes the graph topology.
+ */
+function _visChanged(self)
+{
+    // perform layout if auto layout flag is set
+
+    if (self._autoLayout)
+    {
+        // re-apply layout
+        _performLayout(self);
+    }
+}
+
+/**
+ * Saves the network as a PNG image.
+ */
+function _saveAsPng(self)
+{
+    self._vis.exportNetwork('png', 'export_network.jsp?type=png');
+}
+
+/**
+ * Toggles the visibility of the node labels.
+ */
+function _toggleNodeLabels(self)
+{
+    // update visibility of labels
+
+    self._nodeLabelsVisible = !self._nodeLabelsVisible;
+    self._vis.nodeLabelsVisible(self._nodeLabelsVisible);
+
+    // update check icon of the corresponding menu item
+
+    var item = $(self.mainMenuSelector + " #show_node_labels");
+
+    if (self._nodeLabelsVisible)
+    {
+        item.addClass(self.CHECKED_CLASS);
+    }
+    else
+    {
+        item.removeClass(self.CHECKED_CLASS);
+    }
+}
+
+/**
+ * Toggle auto layout option on or off. If auto layout is active, then the
+ * graph is laid out automatically upon any change.
+ */
+function _toggleAutoLayout(self)
+{
+    // toggle autoLayout option
+
+    self._autoLayout = !self._autoLayout;
+
+    // update check icon of the corresponding menu item
+
+    var item = $(self.settingsDialogSelector + " #auto_layout");
+
+    if (self._autoLayout)
+    {
+        item.addClass(self.CHECKED_CLASS);
+    }
+    else
+    {
+        item.removeClass(self.CHECKED_CLASS);
+    }
+}
+
+/**
+ * Toggle "remove disconnected on hide" option on or off. If this option is
+ * active, then any disconnected node will also be hidden after the hide action.
+ */
+function _toggleRemoveDisconnected(self)
+{
+    // toggle removeDisconnected option
+
+    self._removeDisconnected = !self._removeDisconnected;
+
+    // update check icon of the corresponding menu item
+
+    var item = $(self.mainMenuSelector + " #remove_disconnected");
+
+    if (self._removeDisconnected)
+    {
+        item.addClass(self.CHECKED_CLASS);
+    }
+    else
+    {
+        item.removeClass(self.CHECKED_CLASS);
+    }
+}
+
+/**
+ * Toggles the visibility of the profile data for the nodes.
+ */
+function _toggleProfileData(self)
+{
+    // toggle value and pass to CW
+
+    self._profileDataVisible = !self._profileDataVisible;
+    self._vis.profileDataAlwaysShown(self._profileDataVisible);
+
+    // update check icon of the corresponding menu item
+
+    var item = $(self.mainMenuSelector + " #show_profile_data");
+
+    if (self._profileDataVisible)
+    {
+        item.addClass(self.CHECKED_CLASS);
+    }
+    else
+    {
+        item.removeClass(self.CHECKED_CLASS);
+    }
+}
+
+/**
+ * Highlights the neighbors of the selected nodes.
+ *
+ * The content of this method is copied from GeneMANIA (genemania.org) sources.
+ */
+function _highlightNeighbors(self)
+{
+
+    var nodes = self._vis.selected("nodes");
+
+    if (nodes != null && nodes.length > 0)
+    {
+        var fn = self._vis.firstNeighbors(nodes, true);
+        var neighbors = fn.neighbors;
+        var edges = fn.edges;
+        edges = edges.concat(fn.mergedEdges);
+        neighbors = neighbors.concat(fn.rootNodes);
+        var bypass = self._vis.visualStyleBypass() || {};
+
+        if( ! bypass.nodes )
+        {
+            bypass.nodes = {};
+        }
+        if( ! bypass.edges )
+        {
+            bypass.edges = {};
+        }
+
+        var allNodes = self._vis.nodes();
+
+        $.each(allNodes, function(i, n) {
+            if( !bypass.nodes[n.data.id] ){
+                bypass.nodes[n.data.id] = {};
+            }
+            bypass.nodes[n.data.id].opacity = 0.25;
+        });
+
+        $.each(neighbors, function(i, n) {
+            if( !bypass.nodes[n.data.id] ){
+                bypass.nodes[n.data.id] = {};
+            }
+            bypass.nodes[n.data.id].opacity = 1;
+        });
+
+        var opacity;
+        var allEdges = self._vis.edges();
+        allEdges = allEdges.concat(self._vis.mergedEdges());
+
+        $.each(allEdges, function(i, e) {
+            if( !bypass.edges[e.data.id] ){
+                bypass.edges[e.data.id] = {};
+            }
+
+            opacity = 0.15;
+
+            bypass.edges[e.data.id].opacity = opacity;
+            bypass.edges[e.data.id].mergeOpacity = opacity;
+        });
+
+        $.each(edges, function(i, e) {
+            if( !bypass.edges[e.data.id] ){
+                bypass.edges[e.data.id] = {};
+            }
+
+            opacity = 0.85;
+
+            bypass.edges[e.data.id].opacity = opacity;
+            bypass.edges[e.data.id].mergeOpacity = opacity;
+        });
+
+        self._vis.visualStyleBypass(bypass);
+
+    }
+}
+
+/**
+ * Removes all highlights from the visualization.
+ *
+ * The content of this method is copied from GeneMANIA (genemania.org) sources.
+ */
+function _removeHighlights(self)
+{
+    var bypass = self._vis.visualStyleBypass();
+    bypass.edges = {};
+
+    var nodes = bypass.nodes;
+
+    for (var id in nodes)
+    {
+        var styles = nodes[id];
+        delete styles["opacity"];
+        delete styles["mergeOpacity"];
+    }
+
+    self._vis.visualStyleBypass(bypass);
+}
+
+/**
+ * Finds the non-seed gene having the maximum alteration percent in
+ * the network, and returns the maximum alteration percent value.
+ *
+ * @param map   weight map for the genes in the network
+ * @return      max alteration percent of non-seed genes
+ */
+function _maxAlterValNonSeed(self, map)
+{
+    var max = 0.0;
+
+    for (var key in map)
+    {
+        // skip seed genes
+
+        var node = self._vis.node(key);
+
+        if (node != null &&
+            node.data["IN_QUERY"] == "true")
+        {
+            continue;
+        }
+
+        // update max value if necessary
+        if (map[key] > max)
+        {
+            max = map[key];
+        }
+    }
+
+    return max+1;
+}
+
+function _adjustIE()
+{
+    if (_isIE())
+    {
+        // this is required to position scrollbar on IE
+        //var width = $("#help_tab").width();
+        //$("#help_tab").width(width * 1.15);
+    }
+}
+
+/**
+ * Updates the graphLayout options for CytoscapeWeb.
+ */
+function _updateLayoutOptions(self)
+{
+    // update graphLayout object
+
+    var options = new Object();
+
+    for (var i=0; i < self._layoutOptions.length; i++)
+    {
+        options[self._layoutOptions[i].id] = self._layoutOptions[i].value;
+    }
+
+    self._graphLayout.options = options;
+}
+
+/**
+ * Creates an array containing default option values for the ForceDirected
+ * layout.
+ *
+ * @return  an array of default layout options
+ */
+function _defaultOptsArray()
+{
+    var defaultOpts =
+        [ { id: "gravitation", label: "Gravitation",       value: -350,   tip: "The gravitational constant. Negative values produce a repulsive force." },
+            { id: "mass",        label: "Node mass",         value: 3,      tip: "The default mass value for nodes." },
+            { id: "tension",     label: "Edge tension",      value: 0.1,    tip: "The default spring tension for edges." },
+            { id: "restLength",  label: "Edge rest length",  value: "auto", tip: "The default spring rest length for edges." },
+            { id: "drag",        label: "Drag co-efficient", value: 0.4,    tip: "The co-efficient for frictional drag forces." },
+            { id: "minDistance", label: "Minimum distance",  value: 1,      tip: "The minimum effective distance over which forces are exerted." },
+            { id: "maxDistance", label: "Maximum distance",  value: 10000,  tip: "The maximum distance over which forces are exerted." },
+            { id: "iterations",  label: "Iterations",        value: 400,    tip: "The number of iterations to run the simulation." },
+            { id: "maxTime",     label: "Maximum time",      value: 30000,  tip: "The maximum time to run the simulation, in milliseconds." },
+            { id: "autoStabilize", label: "Auto stabilize",  value: true,   tip: "If checked, layout automatically tries to stabilize results that seems unstable after running the regular iterations." } ];
+
+    return defaultOpts;
+};
+
+/**
+ * Saves layout settings when clicked on the "Save" button of the
+ * "Layout Options" panel.
+ */
+function _saveSettings(self)
+{
+    // update layout option values
+
+    for (var i=0; i < (self._layoutOptions).length; i++)
+    {
+
+        if (self._layoutOptions[i].id == "autoStabilize")
+        {
+            // check if the auto stabilize box is checked
+
+            if($(self.settingsDialogSelector + " #autoStabilize").is(":checked"))
+            {
+                self._layoutOptions[i].value = true;
+                $(self.settingsDialogSelector + " #autoStabilize").val(true);
+            }
+            else
+            {
+                self._layoutOptions[i].value = false;
+                $(self.settingsDialogSelector + " #autoStabilize").val(false);
+            }
+        }
+        else
+        {
+            // simply copy the text field value
+            self._layoutOptions[i].value =
+                $(self.settingsDialogSelector + " #" + self._layoutOptions[i].id).val();
+        }
+    }
+
+    // update graphLayout options
+    _updateLayoutOptions(self);
+
+    // close the settings panel
+    $(self.settingsDialogSelector).dialog("close");
+}
+
+/**
+ * Updates the contents of the layout properties panel.
+ */
+function _updatePropsUI(self)
+{
+    // update settings panel UI
+
+    for (var i=0; i < self._layoutOptions.length; i++)
+    {
+
+        if (self._layoutOptions[i].id == "autoStabilize")
+        {
+            if (self._layoutOptions[i].value == true)
+            {
+                // check the box
+                $(self.settingsDialogSelector + " #autoStabilize").attr("checked", true);
+                $(self.settingsDialogSelector + " #autoStabilize").val(true);
+            }
+            else
+            {
+                // uncheck the box
+                $(self.settingsDialogSelector + " #autoStabilize").attr("checked", false);
+                $(self.settingsDialogSelector + " #autoStabilize").val(false);
+            }
+        }
+        else
+        {
+            $(self.settingsDialogSelector + " #" + self._layoutOptions[i].id).val(
+                self._layoutOptions[i].value);
+        }
+    }
+}
+
+/**
+ * Reverts to default layout settings when clicked on "Default" button of the
+ * "Layout Options" panel.
+ */
+function _defaultSettings(self)
+{
+    self._layoutOptions = _defaultOptsArray();
+    _updateLayoutOptions(self);
+    _updatePropsUI(self);
+}
+
+/**
+ * Sets visibility of the given UI component.
+ *
+ * @param component an html UI component
+ * @param visible   a boolean to set the visibility.
+ */
+function _setComponentVis(component, visible)
+{
+    // set visible
+    if (visible)
+    {
+        if (component.hasClass("hidden-network-ui"))
+        {
+            component.removeClass("hidden-network-ui");
+        }
+    }
+    // set invisible
+    else
+    {
+        if (!component.hasClass("hidden-network-ui"))
+        {
+            component.addClass("hidden-network-ui");
+        }
+    }
+}
+
+/**
+ * Initializes the layout options by default values and updates the
+ * corresponding UI content.
+ */
+function _initLayoutOptions(self)
+{
+    self._layoutOptions = _defaultOptsArray();
+    _updateLayoutOptions(self);
+}
+
+/**
+ * Performs the current layout on the graph.
+ */
+function _performLayout(self)
+{
+    self._vis.layout(self._graphLayout);
+}
+
+/**
+ * Initializes the layout settings panel.
+ */
+function _initPropsUI(self)
+{
+    $(self.settingsDialogSelector + " #fd_layout_settings tr").tipTip();
+}
---
+++ portal/src/main/webapp/js/network/utility-functions.js
@@ -0,0 +1,486 @@
+/*
+ * ##################################################################
+ * ##################### Utility Functions ##########################
+ * ##################################################################
+ */
+
+/**
+ * Parses the given xmlDoc representing the BioGene query result, and
+ * returns a corresponding JSON object.
+ * (Not used anymore, using JSON service of the BioGene instead)
+ *
+ * @param xmlDoc
+ * @private
+ */
+function _parseBioGeneXml(xmlDoc)
+{
+    var json = new Object();
+
+    // check the return code
+    var returnCode = xmlDoc.getElementsByTagName("return_code")[0].childNodes[0].nodeValue;
+
+    json.returnCode = returnCode;
+
+    if(returnCode != "SUCCESS")
+    {
+        return json;
+    }
+
+    // work on the first result only
+    var geneInfo = xmlDoc.getElementsByTagName("gene_info")[0];
+
+    var geneIdNode = geneInfo.getElementsByTagName("gene_id");
+    var geneSymbolNode = geneInfo.getElementsByTagName("gene_symbol");
+    var geneLocationNode = geneInfo.getElementsByTagName("gene_location");
+    var geneChromosomeNode = geneInfo.getElementsByTagName("gene_chromosome");
+    var geneDescriptionNode = geneInfo.getElementsByTagName("gene_description");
+    var geneAliasesNode = geneInfo.getElementsByTagName("gene_aliases");
+    var geneSummaryNode = geneInfo.getElementsByTagName("gene_summary");
+    var geneDesignationsNode = geneInfo.getElementsByTagName("gene_designations");
+    var geneMIMNode = geneInfo.getElementsByTagName("gene_mim");
+
+    if (geneIdNode.length > 0)
+        json.geneId = geneIdNode[0].childNodes[0].nodeValue;
+
+    if (geneSymbolNode.length > 0)
+        json.geneSymbol = geneSymbolNode[0].childNodes[0].nodeValue;
+
+    if (geneLocationNode.length > 0)
+        json.geneLocation = geneLocationNode[0].childNodes[0].nodeValue;
+
+    if (geneChromosomeNode.length > 0)
+        json.geneChromosome = geneChromosomeNode[0].childNodes[0].nodeValue;
+
+    if (geneDescriptionNode.length > 0)
+        json.geneDescription = geneDescriptionNode[0].childNodes[0].nodeValue;
+
+    if (geneAliasesNode.length > 0)
+        json.geneAliases = _parseDelimitedInfo(geneAliasesNode[0].childNodes[0].nodeValue, ":", ",");
+
+    if (geneSummaryNode.length > 0)
+        json.geneSummary = geneSummaryNode[0].childNodes[0].nodeValue;
+
+    if (geneDesignationsNode.length > 0)
+        json.geneDesignations = _parseDelimitedInfo(geneDesignationsNode[0].childNodes[0].nodeValue, ":", ",");
+
+    if (geneMIMNode.length > 0)
+        json.geneMIM = geneMIMNode[0].childNodes[0].nodeValue;
+
+    return json;
+}
+
+/**
+ * Initializes the style of the network menu by adjusting hover behaviour.
+ *
+ * @param divId
+ * @param hoverClass
+ * @private
+ */
+function _initMenuStyle(divId, hoverClass)
+{
+    // Opera fix
+    $("#" + divId + " #network_menu ul").css({display: "none"});
+
+    // adds hover effect to main menu items (File, Topology, View)
+
+    $("#" + divId + " #network_menu li").hover(
+        function() {
+            $(this).find('ul:first').css(
+                {visibility: "visible",display: "none"}).show(400);
+        },
+        function() {
+            $(this).find('ul:first').css({visibility: "hidden"});
+        });
+
+
+    // adds hover effect to menu items
+
+    $("#" + divId + " #network_menu ul a").hover(
+        function() {
+            $(this).addClass(hoverClass);
+        },
+        function() {
+            $(this).removeClass(hoverClass);
+        });
+}
+/**
+ * Comparison function to sort genes alphabetically.
+ *
+ * @param node1	node to compare to node2
+ * @param node2 node to compare to node1
+ * @return 		positive integer if node1 is alphabetically greater than node2
+ * 				negative integer if node2 is alphabetically greater than node1
+ * 				zero if node1 and node2 are alphabetically equal
+ */
+function _geneSort (node1, node2)
+{
+    if (node1.data.label > node2.data.label)
+    {
+        return 1;
+    }
+    else if (node1.data.label < node2.data.label)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+/**
+ * Generates a shortened version of the given node id.
+ *
+ * @param id	id of a node
+ * @return		a shortened version of the id
+ */
+function _shortId(id)
+{
+    var shortId = id;
+
+    if (id.indexOf("#") != -1)
+    {
+        var pieces = id.split("#");
+        shortId = pieces[pieces.length - 1];
+    }
+    else if (id.indexOf(":") != -1)
+    {
+        var pieces = id.split(":");
+        shortId = pieces[pieces.length - 1];
+    }
+
+    return shortId;
+}
+
+/**
+ * Replaces all occurrences of a problematic character with an under dash.
+ * Those characters cause problems with the properties of an HTML object.
+ *
+ * @param str	string to be modified
+ * @return		safe version of the given string
+ */
+function _safeProperty(str)
+{
+    var safeProperty = str;
+
+    safeProperty = _replaceAll(safeProperty, " ", "_");
+    safeProperty = _replaceAll(safeProperty, "/", "_");
+    safeProperty = _replaceAll(safeProperty, "\\", "_");
+    safeProperty = _replaceAll(safeProperty, "#", "_");
+    safeProperty = _replaceAll(safeProperty, ".", "_");
+    safeProperty = _replaceAll(safeProperty, ":", "_");
+    safeProperty = _replaceAll(safeProperty, ";", "_");
+    safeProperty = _replaceAll(safeProperty, '"', "_");
+    safeProperty = _replaceAll(safeProperty, "'", "_");
+
+    return safeProperty;
+}
+
+/**
+ * Replaces all occurrences of the given string in the source string.
+ *
+ * @param source		string to be modified
+ * @param toFind		string to match
+ * @param toReplace		string to be replaced with the matched string
+ * @return				modified version of the source string
+ */
+function _replaceAll(source, toFind, toReplace)
+{
+    var target = source;
+    var index = target.indexOf(toFind);
+
+    while (index != -1)
+    {
+        target = target.replace(toFind, toReplace);
+        index = target.indexOf(toFind);
+    }
+
+    return target;
+}
+
+/**
+ * Checks if the user browser is IE.
+ *
+ * @return	true if IE, false otherwise
+ */
+function _isIE()
+{
+    var result = false;
+
+    if (navigator.appName.toLowerCase().indexOf("microsoft") != -1)
+    {
+        result = true;
+    }
+
+    return result;
+}
+
+/**
+ * Converts the given string to title case format. Also replaces each
+ * underdash with a space.
+ *
+ * @param source	source string to be converted to title case
+ */
+function _toTitleCase(source)
+{
+    var str;
+
+    if (source == null)
+    {
+        return source;
+    }
+
+    // first, trim the string
+    str = source.replace(/\s+$/, "");
+
+    // replace each underdash with a space
+    str = _replaceAll(str, "_", " ");
+
+    // change to lower case
+    str = str.toLowerCase();
+
+    // capitalize starting character of each word
+
+    var titleCase = new Array();
+
+    titleCase.push(str.charAt(0).toUpperCase());
+
+    for (var i = 1; i < str.length; i++)
+    {
+        if (str.charAt(i-1) == ' ')
+        {
+            titleCase.push(str.charAt(i).toUpperCase());
+        }
+        else
+        {
+            titleCase.push(str.charAt(i));
+        }
+    }
+
+    return titleCase.join("");
+}
+
+/*
+ function _transformIntervalValue(value, sourceInterval, targetInterval)
+ {
+ var sourceRange = sourceInterval.end - sourceInterval.start;
+ var targetRange = targetInterval.end - targetInterval.start;
+
+ var transformed = targetInterval.start +
+ (value - sourceInterval.start) * (targetRange / sourceRange);
+
+ return transformed;
+ }
+ */
+
+/**
+ * Finds and returns the maximum value in a given map.
+ *
+ * @param map	map that contains real numbers
+ */
+function _getMaxValue(map)
+{
+    var max = 0.0;
+
+    for (var key in map)
+    {
+        if (map[key] > max)
+        {
+            max = map[key];
+        }
+    }
+
+    return max;
+}
+
+/**
+ * Transforms the input value by using the function:
+ * y = (0.000230926)x^3 - (0.0182175)x^2 + (0.511788)x
+ *
+ * This function is designed to transform slider input, which is between
+ * 0 and 100, to provide a better filtering.
+ *
+ * @param value		input value to be transformed
+ */
+function _transformValue(value)
+{
+    // previous function: y = (0.000166377)x^3 - (0.0118428)x^2 + (0.520007)x
+
+    var transformed = 0.000230926 * Math.pow(value, 3) -
+                      0.0182175 * Math.pow(value, 2) +
+                      0.511788 * value;
+
+    if (transformed < 0)
+    {
+        transformed = 0;
+    }
+    else if (transformed > 100)
+    {
+        transformed = 100;
+    }
+
+    return transformed;
+}
+
+/**
+ * Transforms the given value by solving the equation
+ *
+ *   y = (0.000230926)x^3 - (0.0182175)x^2 + (0.511788)x
+ *
+ * where y = value
+ *
+ * @param value	value to be reverse transformed
+ * @returns		reverse transformed value
+ */
+function _reverseTransformValue(value)
+{
+    // find x, where y = value
+
+    var reverse = _solveCubic(0.000230926,
+                              -0.0182175,
+                              0.511788,
+                              -value);
+
+    return reverse;
+}
+
+/**
+ * Solves the cubic function
+ *
+ *   a(x^3) + b(x^2) + c(x) + d = 0
+ *
+ * by using the following formula
+ *
+ *   x = {q + [q^2 + (r-p^2)^3]^(1/2)}^(1/3) + {q - [q^2 + (r-p^2)^3]^(1/2)}^(1/3) + p
+ *
+ * where
+ *
+ *   p = -b/(3a), q = p^3 + (bc-3ad)/(6a^2), r = c/(3a)
+ *
+ * @param a	coefficient of the term x^3
+ * @param b	coefficient of the term x^2
+ * @param c coefficient of the term x^1
+ * @param d coefficient of the term x^0
+ *
+ * @returns one of the roots of the cubic function
+ */
+function _solveCubic(a, b, c, d)
+{
+    var p = (-b) / (3*a);
+    var q = Math.pow(p, 3) + (b*c - 3*a*d) / (6 * Math.pow(a,2));
+    var r = c / (3*a);
+
+    //alert(q*q + Math.pow(r - p*p, 3));
+
+    var sqrt = Math.pow(q*q + Math.pow(r - p*p, 3), 1/2);
+
+    //var root = Math.pow(q + sqrt, 1/3) +
+    //	Math.pow(q - sqrt, 1/3) +
+    //	p;
+
+    var x = _cubeRoot(q + sqrt) +
+            _cubeRoot(q - sqrt) +
+            p;
+
+    return x;
+}
+
+/**
+ * Evaluates the cube root of the given value. This function also handles
+ * negative values unlike the built-in Math.pow() function.
+ *
+ * @param value	source value
+ * @returns		cube root of the source value
+ */
+function _cubeRoot(value)
+{
+    var root = Math.pow(Math.abs(value), 1/3);
+
+    if (value < 0)
+    {
+        root = -root;
+    }
+
+    return root;
+}
+
+// TODO get the x-coordinate of the event target (with respect to the window).
+function _mouseX(evt)
+{
+    if (evt.pageX)
+    {
+        return evt.pageX;
+    }
+    else if (evt.clientX)
+    {
+        return evt.clientX + (document.documentElement.scrollLeft ?
+            document.documentElement.scrollLeft :
+            document.body.scrollLeft);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+//TODO get the y-coordinate of the event target (with respect to the window).
+function _mouseY(evt)
+{
+    if (evt.pageY)
+    {
+        return evt.pageY;
+    }
+    else if (evt.clientY)
+    {
+        return evt.clientY + (document.documentElement.scrollTop ?
+            document.documentElement.scrollTop :
+            document.body.scrollTop);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+/**
+ * Temporary function for debugging purposes
+ */
+function jokerAction(evt)
+{
+    var node = evt.target;
+    var str = _nodeDetails(node);
+    alert(str);
+}
+
+/**
+ * Temporary function for debugging purposes
+ */
+function _nodeDetails(node)
+{
+    var str = "";
+
+    if (node != null)
+    {
+        str += "fields: ";
+
+        for (var field in node)
+        {
+            str += field + ";";
+        }
+
+        str += "\n";
+        //str += "data len: " + node.data.length " \n";
+        str += "data: \n";
+
+
+        for (var field in node.data)
+        {
+            str += field + ": " +  node.data[field] + "\n";
+        }
+    }
+
+    str += "short id: " + _shortId(node.data.id) + "\n";
+    str += "safe id: " + _safeProperty(node.data.id) + "\n";
+
+    return str;
+}
