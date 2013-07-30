334066877606ccfe496feabf7df936a24d66dbd9
Jianjiong Gao <jianjiong.gao@gmail.com>
2013-07-30 16:39:44 -0400

    import PDB PTM Data

---
+++ core/src/main/java/org/mskcc/cbio/cgds/dao/DaoPdbPtmData.java
@@ -0,0 +1,94 @@
+/** Copyright (c) 2012 Memorial Sloan-Kettering Cancer Center.
+**
+** This library is free software; you can redistribute it and/or modify it
+** under the terms of the GNU Lesser General Public License as published
+** by the Free Software Foundation; either version 2.1 of the License, or
+** any later version.
+**
+** This library is distributed in the hope that it will be useful, but
+** WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+** MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+** documentation provided hereunder is on an "as is" basis, and
+** Memorial Sloan-Kettering Cancer Center 
+** has no obligations to provide maintenance, support,
+** updates, enhancements or modifications.  In no event shall
+** Memorial Sloan-Kettering Cancer Center
+** be liable to any party for direct, indirect, special,
+** incidental or consequential damages, including lost profits, arising
+** out of the use of this software and its documentation, even if
+** Memorial Sloan-Kettering Cancer Center 
+** has been advised of the possibility of such damage.  See
+** the GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with this library; if not, write to the Free Software Foundation,
+** Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+**/
+package org.mskcc.cbio.cgds.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author jgao
+ */
+public class DaoPdbPtmData {
+    private DaoPdbPtmData() {}
+    
+    public static int addPdbPdbData(String pdbId,
+            String chain, String ptm, String residues) {
+        if (!MySQLbulkLoader.isBulkLoad()) {
+            throw new IllegalStateException("only bulk load mode is supported ");
+        } 
+        //  write to the temp file maintained by the MySQLbulkLoader
+        MySQLbulkLoader.getMySQLbulkLoader("pdb_ptm_data").insertRecord(pdbId, chain, ptm, residues);
+
+        // return 1 because normal insert will return 1 if no error occurs
+        return 1;
+    }
+    
+    /**
+     * Retrieve ptm for a set of residues to the ptm description in a PDB chain
+     * @param pdbId
+     * @param chain
+     * @param residues
+     * @return Map<set <contacting residues>, ptm description>
+     */
+    public static Map<Set<Integer>,String> getPdbPtmModules(String pdbId, String chain) throws DaoException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = JdbcUtil.getDbConnection(DaoProteinContactMap.class);
+            String sql = "SELECT  `RESIDUES`, `PTM` "
+                    + "FROM  `pdb_ptm_data` "
+                    + "WHERE `PDB_ID`='" + pdbId + "' "
+                    + "AND `CHAIN`='" + chain + "' ";
+            pstmt = con.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+            
+            Map<Set<Integer>,String> map = new HashMap<Set<Integer>,String>();
+            
+            while (rs.next()) {
+                Set<Integer> residues = new HashSet<Integer>();
+                for (String str : rs.getString(1).split(",")) {
+                    residues.add(Integer.parseInt(str));
+                }
+                map.put(residues, rs.getString(2));
+            }
+            
+            return map;
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        } finally {
+            JdbcUtil.closeAll(DaoPdbPtmData.class, con, pstmt, rs);
+        }
+    }
+}
---
+++ core/src/main/java/org/mskcc/cbio/cgds/scripts/ImportPdbPtmData.java
@@ -0,0 +1,105 @@
+/** Copyright (c) 2012 Memorial Sloan-Kettering Cancer Center.
+**
+** This library is free software; you can redistribute it and/or modify it
+** under the terms of the GNU Lesser General Public License as published
+** by the Free Software Foundation; either version 2.1 of the License, or
+** any later version.
+**
+** This library is distributed in the hope that it will be useful, but
+** WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+** MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+** documentation provided hereunder is on an "as is" basis, and
+** Memorial Sloan-Kettering Cancer Center 
+** has no obligations to provide maintenance, support,
+** updates, enhancements or modifications.  In no event shall
+** Memorial Sloan-Kettering Cancer Center
+** be liable to any party for direct, indirect, special,
+** incidental or consequential damages, including lost profits, arising
+** out of the use of this software and its documentation, even if
+** Memorial Sloan-Kettering Cancer Center 
+** has been advised of the possibility of such damage.  See
+** the GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with this library; if not, write to the Free Software Foundation,
+** Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+**/
+package org.mskcc.cbio.cgds.scripts;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.lang.StringUtils;
+import org.mskcc.cbio.cgds.dao.DaoException;
+import org.mskcc.cbio.cgds.dao.DaoPdbPtmData;
+import org.mskcc.cbio.cgds.dao.MySQLbulkLoader;
+import org.mskcc.cbio.cgds.util.ConsoleUtil;
+import org.mskcc.cbio.cgds.util.FileUtil;
+import org.mskcc.cbio.cgds.util.ProgressMonitor;
+
+/**
+ *
+ * @author jgao
+ */
+public class ImportPdbPtmData {
+    
+    public static void importData(File file, ProgressMonitor pMonitor) throws IOException, DaoException {
+        Pattern patternRes = Pattern.compile("[A-Z]{3}:(-?[0-9]+)");
+        MySQLbulkLoader.bulkLoadOn();
+        FileReader reader = new FileReader(file);
+        BufferedReader buf = new BufferedReader(reader);
+        String line;
+        while ((line = buf.readLine()) != null) {
+            if (pMonitor != null) {
+                pMonitor.incrementCurValue();
+                ConsoleUtil.showProgress(pMonitor);
+            }
+            if (!line.startsWith("#")) {
+                String parts[] = line.split("\t");
+                String pdbId = parts[0];
+                String chainId = parts[1];
+                String ptm = parts[2];
+                
+                // residues
+                TreeSet<Integer> residues = new TreeSet<Integer>();
+                for (String res : parts[3].split(",")) {
+                    Matcher m = patternRes.matcher(res);
+                    if (!m.matches()) {
+                        continue;
+                    }
+                    residues.add(Integer.parseInt(m.group(1)));
+                }
+                
+                DaoPdbPtmData.addPdbPdbData(pdbId, chainId, ptm,
+                        StringUtils.join(residues, ","));
+            }
+        }
+        if (MySQLbulkLoader.isBulkLoad()) {
+           MySQLbulkLoader.flushAll();
+        }        
+    }
+    
+    public static void main(String[] args) throws Exception {
+        args = new String[] {"/Users/jj/projects/cbio-portal-data/reference-data/pdb-ptms.txt"};
+        
+        if (args.length == 0) {
+            System.out.println("command line usage:  ImportPdbPtmData.pl <pdb-ptms.txt>");
+            System.exit(1);
+        }
+        ProgressMonitor pMonitor = new ProgressMonitor();
+        pMonitor.setConsoleMode(true);
+
+        File file = new File(args[0]);
+        System.out.println("Reading data from:  " + file.getAbsolutePath());
+        int numLines = FileUtil.getNumLines(file);
+        System.out.println(" --> total number of lines:  " + numLines);
+        pMonitor.setMaxValue(numLines);
+        importData(file, pMonitor);
+        ConsoleUtil.showWarnings(pMonitor);
+        System.err.println("Done.");
+    }
+}
