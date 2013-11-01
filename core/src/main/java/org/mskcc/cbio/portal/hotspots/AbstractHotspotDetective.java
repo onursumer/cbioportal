022bb853590351e6fd671c6cfdf9f8a514bfb7c3
Jianjiong Gao <jianjiong.gao@gmail.com>
2013-11-01 16:00:59 -0400

    move length funcitons etc back to abstract class

--- core/src/main/java/org/mskcc/cbio/portal/hotspots/AbstractHotspotDetective.java
+++ core/src/main/java/org/mskcc/cbio/portal/hotspots/AbstractHotspotDetective.java
@@ -26,6 +26,8 @@
 **/
 package org.mskcc.cbio.portal.hotspots;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -37,11 +39,21 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.lang.StringUtils;
+import org.mskcc.cbio.portal.dao.DaoException;
 import org.mskcc.cbio.portal.dao.DaoGeneOptimized;
+import org.mskcc.cbio.portal.dao.DaoPfamGraphics;
 import org.mskcc.cbio.portal.dao.JdbcUtil;
 import org.mskcc.cbio.portal.model.CanonicalGene;
 import org.mskcc.cbio.portal.model.ExtendedMutation;
+import org.mskcc.cbio.portal.web_api.ConnectionManager;
 
 /**
  *
@@ -83,10 +95,6 @@ public abstract class AbstractHotspotDetective implements HotspotDetective {
      * @return 
      */
     protected abstract Set<Hotspot> processSingleHotspotsOnAProtein(Set<Hotspot> hotspotsOnAProtein);
-    
-    protected abstract int getNumberOfAllMutationOnProtein(Set<Hotspot> hotspotsOnAProtein);
-    
-    protected abstract int getLengthOfProtein(Set<Hotspot> hotspotsOnAProtein);
     
     @Override
     public void detectHotspot() throws HotspotException {
@@ -204,5 +212,116 @@ public abstract class AbstractHotspotDetective implements HotspotDetective {
             throw new java.lang.IllegalStateException("No detection has ran.");
         }
         return Collections.unmodifiableSet(hotspots);
+    }
+    
+    protected int getNumberOfAllMutationOnProtein(Set<Hotspot> hotspotsOnAProtein) {
+	// default behavior for single and linear hotspots
+        Set<ExtendedMutation> mutations = new HashSet<ExtendedMutation>();
+        for (Hotspot hotspot : hotspotsOnAProtein) {
+            mutations.addAll(hotspot.getMutations());
+        }
+        return mutations.size();
+    }
+    
+    
+    protected int getLengthOfProtein(Set<Hotspot> hotspotsOnAProtein) {
+	// default behavior for single and linear hotspots
+        MutatedProtein protein = hotspotsOnAProtein.iterator().next().getProtein();
+        int length = getProteinLength(protein.getUniprotAcc());
+        
+        if (length==0) {
+            // TODO: this is not ideal -- under estimating p values
+            length = getLargestResidue(hotspotsOnAProtein);
+        }
+        
+        return length;
+    }
+    
+    private static int getLargestResidue(Set<Hotspot> hotspotsOnAProtein) {
+        int length = 0;
+        for (Hotspot hotspot : hotspotsOnAProtein) {
+            int residue = hotspot.getResidues().last();
+            if (residue>length) {
+                length = residue;
+            }
+        }
+        return length;
+    }
+    
+    private static Map<String, Integer> mapUniprotProteinLengths = null;
+    private static int getProteinLength(String uniprotAcc) {
+        if (mapUniprotProteinLengths == null) {
+            mapUniprotProteinLengths = new HashMap<String, Integer>();
+            
+            Map<String, String> pfamGraphics;
+            try {
+                pfamGraphics = DaoPfamGraphics.getAllPfamGraphics();
+            } catch (DaoException e) {
+                e.printStackTrace();
+                return -1;
+            }
+            
+            Pattern p = Pattern.compile("\"length\":\"([0-9]+)\"");
+            
+            for (Map.Entry<String, String> entry : pfamGraphics.entrySet()) {
+                String uni = entry.getKey();
+                String json = entry.getValue();
+                Matcher m = p.matcher(json);
+                if (m.find()) {
+                    Integer length = Integer.valueOf(m.group(1));
+                    mapUniprotProteinLengths.put(uni, length);
+                }
+            }
+        }
+        
+        Integer ret = mapUniprotProteinLengths.get(uniprotAcc);
+        
+        if (ret==null) {
+            ret = getProteinLengthFromUniprot(uniprotAcc);
+            mapUniprotProteinLengths.put(uniprotAcc, ret);
+        }
+        
+        if (ret==0) {
+            System.out.println("No length informaiton found for "+uniprotAcc);
+            return 0;
+        }
+        
+        return ret;
+    }
+    
+    private static int getProteinLengthFromUniprot(String uniprotAcc) {
+        String strURL = "http://www.uniprot.org/uniprot/"+uniprotAcc+".fasta";
+        MultiThreadedHttpConnectionManager connectionManager =
+                ConnectionManager.getConnectionManager();
+        HttpClient client = new HttpClient(connectionManager);
+        GetMethod method = new GetMethod(strURL);
+        
+        try {
+            int statusCode = client.executeMethod(method);
+            if (statusCode == HttpStatus.SC_OK) {
+                BufferedReader bufReader = new BufferedReader(
+                        new InputStreamReader(method.getResponseBodyAsStream()));
+                String line = bufReader.readLine();
+                if (line==null||!line.startsWith(">")) {
+                    return 0;
+                }
+                
+                int len = 0;
+                for (line=bufReader.readLine(); line!=null; line=bufReader.readLine()) {
+                    len += line.length();
+                }
+                return len;
+            } else {
+                //  Otherwise, throw HTTP Exception Object
+                throw new HttpException(statusCode + ": " + HttpStatus.getStatusText(statusCode)
+                        + " Base URL:  " + strURL);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        } finally {
+            //  Must release connection back to Apache Commons Connection Pool
+            method.releaseConnection();
+        }
     }
 }
--- core/src/main/java/org/mskcc/cbio/portal/hotspots/SingleHotspotDetective.java
+++ core/src/main/java/org/mskcc/cbio/portal/hotspots/SingleHotspotDetective.java
@@ -26,25 +26,9 @@
 **/
 package org.mskcc.cbio.portal.hotspots;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.mskcc.cbio.portal.dao.DaoException;
-import org.mskcc.cbio.portal.dao.DaoPfamGraphics;
-import org.mskcc.cbio.portal.model.ExtendedMutation;
-import org.mskcc.cbio.portal.web_api.ConnectionManager;
 
 /**
  *
@@ -74,115 +58,5 @@ public class SingleHotspotDetective extends AbstractHotspotDetective {
             }
         }
         return ret;
-    }
-    
-    @Override
-    protected int getNumberOfAllMutationOnProtein(Set<Hotspot> hotspotsOnAProtein) {
-        Set<ExtendedMutation> mutations = new HashSet<ExtendedMutation>();
-        for (Hotspot hotspot : hotspotsOnAProtein) {
-            mutations.addAll(hotspot.getMutations());
-        }
-        return mutations.size();
-    }
-    
-    @Override
-    protected int getLengthOfProtein(Set<Hotspot> hotspotsOnAProtein) {
-        MutatedProtein protein = hotspotsOnAProtein.iterator().next().getProtein();
-        int length = getProteinLength(protein.getUniprotAcc());
-        
-        if (length==0) {
-            // TODO: this is not ideal -- under estimating p values
-            length = getLargestResidue(hotspotsOnAProtein);
-        }
-        
-        return length;
-    }
-    
-    private static int getLargestResidue(Set<Hotspot> hotspotsOnAProtein) {
-        int length = 0;
-        for (Hotspot hotspot : hotspotsOnAProtein) {
-            int residue = hotspot.getResidues().last();
-            if (residue>length) {
-                length = residue;
-            }
-        }
-        return length;
-    }
-    
-    private static Map<String, Integer> mapUniprotProteinLengths = null;
-    private static int getProteinLength(String uniprotAcc) {
-        if (mapUniprotProteinLengths == null) {
-            mapUniprotProteinLengths = new HashMap<String, Integer>();
-            
-            Map<String, String> pfamGraphics;
-            try {
-                pfamGraphics = DaoPfamGraphics.getAllPfamGraphics();
-            } catch (DaoException e) {
-                e.printStackTrace();
-                return -1;
-            }
-            
-            Pattern p = Pattern.compile("\"length\":\"([0-9]+)\"");
-            
-            for (Map.Entry<String, String> entry : pfamGraphics.entrySet()) {
-                String uni = entry.getKey();
-                String json = entry.getValue();
-                Matcher m = p.matcher(json);
-                if (m.find()) {
-                    Integer length = Integer.valueOf(m.group(1));
-                    mapUniprotProteinLengths.put(uni, length);
-                }
-            }
-        }
-        
-        Integer ret = mapUniprotProteinLengths.get(uniprotAcc);
-        
-        if (ret==null) {
-            ret = getProteinLengthFromUniprot(uniprotAcc);
-            mapUniprotProteinLengths.put(uniprotAcc, ret);
-        }
-        
-        if (ret==0) {
-            System.out.println("No length informaiton found for "+uniprotAcc);
-            return 0;
-        }
-        
-        return ret;
-    }
-    
-    private static int getProteinLengthFromUniprot(String uniprotAcc) {
-        String strURL = "http://www.uniprot.org/uniprot/"+uniprotAcc+".fasta";
-        MultiThreadedHttpConnectionManager connectionManager =
-                ConnectionManager.getConnectionManager();
-        HttpClient client = new HttpClient(connectionManager);
-        GetMethod method = new GetMethod(strURL);
-        
-        try {
-            int statusCode = client.executeMethod(method);
-            if (statusCode == HttpStatus.SC_OK) {
-                BufferedReader bufReader = new BufferedReader(
-                        new InputStreamReader(method.getResponseBodyAsStream()));
-                String line = bufReader.readLine();
-                if (line==null||!line.startsWith(">")) {
-                    return 0;
-                }
-                
-                int len = 0;
-                for (line=bufReader.readLine(); line!=null; line=bufReader.readLine()) {
-                    len += line.length();
-                }
-                return len;
-            } else {
-                //  Otherwise, throw HTTP Exception Object
-                throw new HttpException(statusCode + ": " + HttpStatus.getStatusText(statusCode)
-                        + " Base URL:  " + strURL);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return 0;
-        } finally {
-            //  Must release connection back to Apache Commons Connection Pool
-            method.releaseConnection();
-        }
     }
 }
