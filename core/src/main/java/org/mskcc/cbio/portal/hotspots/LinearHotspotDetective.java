91cce9bcc22354827c3f75a7ac7ab0bb76ec3f2a
Jianjiong Gao <jianjiong.gao@gmail.com>
2013-12-06 14:13:22 -0500

    LinearHotspotDetective

---
+++ core/src/main/java/org/mskcc/cbio/portal/hotspots/LinearHotspotDetective.java
@@ -0,0 +1,142 @@
+/** Copyright (c) 2012 Memorial Sloan-Kettering Cancer Center.
+**
+** This library is free software; you can redistribute it and/or modify it
+** under the terms of the GNU Lesser General Public License as published
+** by the Free Software Foundation; either version 2.1 of the License, or
+** any later version.
+**
+** This library is distributed in the hope that it will be useful, but
+** WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+** MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+** documentation provided hereunder is on an "as is" basis, and
+** Memorial Sloan-Kettering Cancer Center 
+** has no obligations to provide maintenance, support,
+** updates, enhancements or modifications.  In no event shall
+** Memorial Sloan-Kettering Cancer Center
+** be liable to any party for direct, indirect, special,
+** incidental or consequential damages, including lost profits, arising
+** out of the use of this software and its documentation, even if
+** Memorial Sloan-Kettering Cancer Center 
+** has been advised of the possibility of such damage.  See
+** the GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with this library; if not, write to the Free Software Foundation,
+** Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+**/
+package org.mskcc.cbio.portal.hotspots;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author jgao
+ */
+public class LinearHotspotDetective extends AbstractHotspotDetective {
+    private int thresholdSamples;
+    private int window;
+
+    public LinearHotspotDetective(Collection<Integer> cancerStudyIds,
+            int thresholdSamples, int window) {
+        super(cancerStudyIds);
+        this.thresholdSamples = thresholdSamples;
+        this.window = window;
+    }
+    
+    /**
+     *
+     * @param hotspots
+     * @return
+     */
+    @Override
+    protected Set<Hotspot> processSingleHotspotsOnAProtein(Set<Hotspot> hotspotsOnAProtein) {
+        List<Integer> hotspotCenters = findLocalMaximum(hotspotsOnAProtein);
+        Set<Hotspot> ret = new HashSet<Hotspot>();
+        for (int center : hotspotCenters) {
+            
+        }
+        
+        
+        return ret;
+    }
+    
+    
+    
+    /**
+     * 
+     * @param mapPositionSamples Map<residue position, # samples>
+     * @param lenProtein protein length or maximum position mutated
+     * @param window if window=2, we take 2 upstream and 2 downstream residues
+     * @param threshold samples threshold
+     * @return 
+     */
+    private List<Integer> findLocalMaximum(Set<Hotspot> hotspotsOnAProtein) {
+        
+        //arrSamples e.g. 0044400, 0040400, 00400, 004400
+        int[] arrSamples = sampleCountArrayAlongProtein(hotspotsOnAProtein, window);
+        int[] sumWindow = sumInWindow(arrSamples, window);
+        
+        List<Integer> list = new ArrayList<Integer>();
+        
+        int plateauStart = -1;
+        for (int i=1; i<arrSamples.length; i++) {
+            if (sumWindow[i]>=thresholdSamples) {
+                if (sumWindow[i]>sumWindow[i-1]) {
+                    plateauStart = i;
+                } else if (sumWindow[i]==sumWindow[i-1]) {
+                    // if equal, no change to plateauStart
+                } else if (plateauStart != -1) { // sumWindow[i]<sumWindow[i-1]
+                    list.add((plateauStart+i-1)/2); // add the middle point
+                    plateauStart = -1;
+                } 
+            } else if (plateauStart != -1) {
+                list.add((plateauStart+i-1)/2); // add the middle point
+                plateauStart = -1;
+            }
+        }
+ 
+//        for (int i=1; i<lenProtein-1; i++) {
+//            if (sumWindow[i]>=threshold && ( (sumWindow[i]>sumWindow[i-1] || (sumWindow[i]==sumWindow[i-1] && arrSamples[i]>arrSamples[i-1])) &&
+//                     (sumWindow[i]>sumWindow[i+1] || (sumWindow[i]==sumWindow[i+1] && arrSamples[i]>arrSamples[i+1]))) ) {
+//                list.add(i);
+//            }
+//        }
+
+        
+        return list;
+    }
+    
+    private int[] sampleCountArrayAlongProtein(Set<Hotspot> hotspotsOnAProtein, int window) {
+        int lenProtein = hotspotsOnAProtein.iterator().next().getProtein().getProteinLength();
+        int[] array = new int[lenProtein+window];
+        for (Hotspot hotspot : hotspotsOnAProtein) {
+            array[hotspot.getResidues().first()] += hotspot.getSamples().size();
+        }
+        return array;
+    }
+    
+    private static int[] sumInWindow(int[] arr, int window) {
+        int len = arr.length;
+        int[] sum = new int[len];
+        int last = 0;
+        for (int i=0; i<len && i<window; i++) {
+            last += arr[i];
+        }
+        
+        for (int i=0; i<len; i++) {
+            if (i>window+1) {
+                last -= arr[i-window-1];
+            }
+            if (i+window<len) {
+                last += arr[i+window];
+            }
+            sum[i] = last;
+        }
+        return sum;
+    }
+}
